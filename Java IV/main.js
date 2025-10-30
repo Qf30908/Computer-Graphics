2f288c3f275ad3626a12d47347e3dc9e991a652c
Qf30908 <147955058+Qf30908@users.noreply.github.com>
2025-10-30 13:11:57 +0100

    switching Perspective to Orthographic Camera

--- Java IV/main.js
+++ Java IV/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { contain } from 'three/src/extras/TextureUtils.js';
 
-// Scene 1
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -9,40 +9,30 @@ const camera = new THREE.PerspectiveCamera(
     window.innerWidth / window.innerHeight, 
     0.1, 
     1000
-);
+)
 camera.position.set(0, 0, 5);
 
-const boxGeometry = new THREE.BoxGeometry(1,1,1);
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMaterial = new THREE.MeshBasicMaterial({ 
-    color: 0x00ff00
-});
-
+        color: 0x00ff00
+    });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+boxMesh.position.set(-2, 0, -2);
 
-boxMeesh.position.set(-2, 0, -2);
 scene.add(boxMesh);
 
 const box1Material = new THREE.MeshBasicMaterial({ 
-    color: 0x0000ff
-});
-
-const box1Mesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMeesh.position.set(2, 0, 0);
-
+        color: 0x0000ff
+    });
+const box1Mesh = new THREE.Mesh(boxGeometry, box1Material);
+box1Mesh.position.set(2, 0, 0);
 scene.add(box1Mesh);
 
-
-const light = new THREE.AmbientLight(0xffffff, 1); 
+const light = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light);
-
 
 const container = document.getElementById('scene1');
 const renderer = new THREE.WebGLRenderer();
-
 container.appendChild(renderer.domElement);
 
 renderer.render(scene, camera);
-
-
-
-
